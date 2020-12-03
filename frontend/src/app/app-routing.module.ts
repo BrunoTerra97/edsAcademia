07b5b3972c0224e4740d92b51c0cfcab7645cc7d
Rafael Schittini <rafael.schittini@pris.com.br>
2020-12-02 22:07:51 -0300

    foi boi vsf

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -2,19 +2,23 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { MedicoComponent } from './medico/medico.component';
+import { ProfessorComponent } from './professor/professor.component';
 import { SecretariaComponent } from './secretaria/secretaria.component';
 import { TesteComponent } from './teste/teste.component';
+import { UserPageComponent } from './user-page/user-page.component';
 
 const routes: Routes = [
   { path: 'app-login', component: LoginComponent },
   { path: 'app-medico', component: MedicoComponent },
   { path: 'app-secretaria', component: SecretariaComponent },
   { path: 'app-teste', component: TesteComponent },
-  { path: '**', redirectTo: '/app-login', pathMatch: 'full' }
+  { path: 'app-user', component: UserPageComponent },
+  { path: 'app-professor', component: ProfessorComponent },
+  { path: '**', redirectTo: '/app-login', pathMatch: 'full' },
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
