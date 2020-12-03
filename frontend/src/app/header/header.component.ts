97497a03d873e1ce5c7e974cffe36842e041f1bb
Rafael Schittini <rafael.schittini@pris.com.br>
2020-12-02 23:24:01 -0300

    login

--- frontend/src/app/header/header.component.ts
+++ frontend/src/app/header/header.component.ts
@@ -1,15 +1,25 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
+  styleUrls: ['./header.component.scss'],
 })
 export class HeaderComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(private router: Router, private route: ActivatedRoute) {}
+  logoutDisabled = false;
+  logout() {
+    this.router.navigate(['/app-login']);
   }
-
+  ngOnInit(): void {
+    this.router.events.subscribe((url: any) => {
+      console.log(url.url);
+      if (url.url === '/app-login') {
+        this.logoutDisabled = true;
+      } else if (url.url != null) {
+        this.logoutDisabled = false;
+      }
+    });
+  }
 }
--- frontend/src/app/services/geral.service.ts
+++ frontend/src/app/services/geral.service.ts
@@ -8,6 +8,11 @@ export class GeralService {
   constructor(private http: HttpClient) {}
 
   getUser() {
-    return this.http.get('url');
+    return this.http.get('localhost:3333/usuario');
+  }
+
+  login(login: any) {
+    console.log(login);
+    return this.http.post('http://localhost:3333/usuarioGet', login);
   }
 }
